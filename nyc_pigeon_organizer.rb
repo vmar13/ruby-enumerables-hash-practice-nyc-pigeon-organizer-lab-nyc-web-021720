d60f1c0bfbbe20f4eba6c0c4dac690edc7d58c8c
Vanessa Martinez <vmar76@gmail.com>
2020-02-15 05:34:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |colors_gender_lives, values|
+    values.each do |value, array|
+      array.each do |name|
+        if new_hash[name] == nil 
+          new_hash[name] = {}
+          new_hash[name][colors_gender_lives] = []
+        else
+          new_hash[name][colors_gender_lives] = []
+        end
+      end
+    end 
 end
+new_hash.each do |name, values|
+values.each do |hashvalue, array|
+  data.each do |colors_gender_lives, values|
+    values.each do |value, array|
+      
+      array.each do |element|
+        if element == name && hashvalue == colors_gender_lives
+          
+          new_hash[name][hashvalue] << value. to_s 
+        end
+      end
+    end
+  end
+end
+end
+new_hash
+end 
