0696e4c7383b964149fd6bb1530302ce59b65ccd
Vanessa Martinez <vmar76@gmail.com>
2020-02-15 05:14:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,3 @@ def nyc_pigeon_organizer(data)
   data.each do |colors, gender, lives|
 end
 
-# 
-#       it "returns a hash with seven keys" do
-#         expect(@result.keys.length).to eq(7)
-#       end
-#       it 'has the keys Theo, Peter Jr., Lucky, Ms. K, Queenie, Andrew, and Alex' do
-#         names = ["Theo", "Peter Jr.", "Lucky", "Ms. K", "Queenie", "Andrew", "Alex"]
-#         names.each { |n| expect(@result.keys).to include(n) }
-#       end
-#     end
