c4765ba9a0eb67782f43c80323c337d736856317
Vanessa Martinez <vmar76@gmail.com>
2020-02-15 05:20:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,14 @@
 def nyc_pigeon_organizer(data)
-  result = {}
-  data.each do |colors, gender, lives|
+  new_hash = {}
+  data.each do |colors_gender_lives, values|
+    values.each do |value, array|
+      array.each do |name|
+        if new_hash[name] == nil 
+          new_hash[name] = {}
+          new_hash[name][colors_gender_lives] = []
+        end
+      end
+    end 
 end
+
 
